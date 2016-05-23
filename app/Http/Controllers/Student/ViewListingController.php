0618fb89e1e83965c1c7013b59ea211544c14f28
Yuzrie Siddiq <yuyieyuzrie@gmail.com>
2016-05-23 20:47:05 +0800

    modified the controllers to make the pages viewable (removed App\UnitListing and App\StudyPlanner)

--- app/Http/Controllers/Student/ViewListingController.php
+++ app/Http/Controllers/Student/ViewListingController.php
@@ -7,22 +7,27 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use App\Student;
-use App\UnitListing;
+use App\Unit;
 use DB;
 
 class ViewListingController extends Controller
 {
     public function index() {
         // $student = Student::findOrFail($stdID);  // this may need extra parameter
-        $unitlisting = DB::table('unit_listing')
-            ->join('unit', 'unit_listing.unitCode', '=', 'unit.unitCode')
-            ->select('unit_listing.*', 'unit.unitName')
-            ->get();          // this need to filter based on student's course
 
-        $data = [];
+        // commented out on 23/05/22016
+        // $unitlisting = DB::table('unit_listing')
+        //     ->join('unit', 'unit_listing.unitCode', '=', 'unit.unitCode')
+        //     ->select('unit_listing.*', 'unit.unitName')
+        //     ->get();          // this need to filter based on student's course
+        //
+        // $data = [];
+
+        // for now need at least something to pass to the $data to make the page work
+        $unitlisting = Unit::all();
         $data['units'] = $unitlisting;
 
         return view ('student.viewunitlistings', $data);
+        // return view ('student.viewunitlistings');
     }
 }
--- app/Http/Controllers/Student/ViewPlannerController.php
+++ app/Http/Controllers/Student/ViewPlannerController.php
@@ -7,21 +7,28 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use App\Student;
-use App\StudyPlanner;
+use App\Unit;
 use DB;
 
 class ViewPlannerController extends Controller
 {
     public function index() {
         // $student = Student::findOrFail($stdID);  // this may need extra parameter
-        $unitlisting = DB::table('unit_listing')
-            ->join('unit', 'unit.unitCode', '=', 'unit_listing.unitCode')
-            ->select('unit_listing.*', 'unit.unitName', 'unit.prerequisite')
-            ->get();         // this need to filter based on student's course
 
-        $data = [];
+        // commented out on 23/05/22016
+        // $unitlisting = DB::table('unit_listing')
+        //     ->join('unit', 'unit.unitCode', '=', 'unit_listing.unitCode')
+        //     ->select('unit_listing.*', 'unit.unitName', 'unit.prerequisite')
+        //     ->get();         // this need to filter based on student's course
+        //
+        // $data = [];
+        // $data['units'] = $unitlisting;
+
+        // for now need at least something to pass to the $data to make the page work
+        $unitlisting = Unit::all();
         $data['units'] = $unitlisting;
+
         return view ('student.viewstudyplanner', $data);
+        // return view ('student.viewstudyplanner');
     }
 }

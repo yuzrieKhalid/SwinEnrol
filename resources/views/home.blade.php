f94ef64b7f515db100bcda3b47741d1e9a2e2561
JasonTC <j450n.tc@gmail.com>
2016-05-24 00:35:03 +0800

    home page manage units link pointing to wrong page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="col-md-3">
             <div class="list-group">
                 <a href="{{ url('/student') }}" class="list-group-item active">Home</a>
-                <a href="{{ url('/student/manageunits') }}" class="list-group-item">Manage Units</a>
+                <a href="{{ url('/student/manageunits/create') }}" class="list-group-item">Manage Units</a>
                 <a href="{{ url('/student/viewstudyplanner') }}" class="list-group-item">View Study Planner</a>
                 <a href="{{ url('/student/viewunitlistings') }}" class="list-group-item">View Unit Listings</a>
                 <a href="{{ url('/student/internalcoursetransfer') }}" class="list-group-item">Internal Course Transfer</a>

cb489ba01816f6fdd2416792ae62e534330fa5e2
Jason <j450n.tc@gmail.com>
2016-08-16 03:44:10 +0800

    coordinator/manageunitlisting: separated lists for long/short semesters, add/remove units to unit list, student/viewunitlistings: separated lists for long/short semesters

--- app/Http/Controllers/Student/ViewListingController.php
+++ app/Http/Controllers/Student/ViewListingController.php
@@ -7,25 +7,23 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use App\Unit;
+use App\UnitTerm;
 use DB;
 
 class ViewListingController extends Controller
 {
     public function index() {
-        // $student = Student::findOrFail($stdID);  // this may need extra parameter
+        $units = UnitTerm::with('unit', 'unit_type')
+            ->where('unitType', '=', 'unit_listing')
+            ->where('enrolmentTerm', '=', '0')
+            ->get();
+        $data['termUnits'] = $units;
 
-        // commented out on 23/05/22016
-        // $unitlisting = DB::table('unit_listing')
-        //     ->join('unit', 'unit_listing.unitCode', '=', 'unit.unitCode')
-        //     ->select('unit_listing.*', 'unit.unitName')
-        //     ->get();          // this need to filter based on student's course
-        //
-        // $data = [];
-
-        // for now need at least something to pass to the $data to make the page work
-        $unitlisting = Unit::all();
-        $data['units'] = $unitlisting;
+        $units = UnitTerm::with('unit', 'unit_type')
+            ->where('unitType', '=', 'unit_listing')
+            ->where('enrolmentTerm', '=', '1')
+            ->get();
+        $data['termUnitsShort'] = $units;
 
         return view ('student.viewunitlistings', $data);
         // return view ('student.viewunitlistings');

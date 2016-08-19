09f21b3983d38f40d4edcd74fa414b7d91b8238d
Mohamad Yuzrie Khalid <yuyieyuzrie@gmail.com>
2016-08-20 02:14:16 +0800

    fixed the CREATE for Manage Student

--- resources/views/admin/managestudents.blade.php
+++ resources/views/admin/managestudents.blade.php
@@ -68,234 +68,37 @@
 
                             <form class="form" method="POST" action="{{ url('/admin/managestudents') }}">
                                 <div class="modal-body">
-                                    <!-- Student Personal Details  -->
-                                    <h3>Personal Details</h3>
                                     <div class="form-group">
-                                        <label for="gender">Gender: </label>
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Gender <span class="caret"></span>
-                                            </button>
-                                            <ul class="dropdown-menu">
-                                                <li><a href="#">Male</a></li>
-                                                <li><a href="#">Female</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-
-                                    <div id="dateOfBirth">
-                                        <label for="dateOfBirth">Date of Birth: </label>
-                                        <div class="input-group date">
-                                            <input type="text" class="form-control" >
-                                            <div class="input-group-addon">
-                                                <span class="glyphicon glyphicon-th"></span>
-                                            </div>
-                                        </div>
+                                        <label>Student ID:</label>
+                                        <input class="form-control" id="studentID" placeholder="4318595">
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="surname">Family Name / Surname:</label>
-                                        <input type="text" class="form-control" id="surname" placeholder="John">
+                                        <label>Family Name / Surname:</label>
+                                        <input type="text" class="form-control" id="surname" placeholder="Doe">
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="givenName">Given Name:</label>
-                                        <input type="text" class="form-control" id="givenName" placeholder="Doe">
+                                        <label>Given Name:</label>
+                                        <input type="text" class="form-control" id="givenName" placeholder="John">
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="email">Email:</label>
+                                        <label>Email:</label>
                                         <input type="email" class="form-control" id="email" placeholder="example.com">
                                     </div>
 
-                                    <!-- Overseas Address Details -->
-                                    <h3>Overseas Address Details</h3>
                                     <div class="form-group">
-                                        <label for="overseasAddress">Overseas Address:</label>
-                                        <textarea class="form-control" id="overseasAddress" rows="2"></textarea>
+                                        <label>Course Code:</label>
+                                        <input type="text" class="form-control" id="courseCode" placeholder="IO47">
                                     </div>
-
-                                    <div class="form-group">
-                                        <label for="overseasCountry">Overseas Country:</label>
-                                        <input type="text" class="form-control" id="overseasCountry" placeholder="United States">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="overseasPostcode">Overseas Postcode:</label>
-                                        <input type="text" class="form-control" id="overseasPostcode" placeholder="129320">
-                                    </div>
-
-                                    <!-- Malaysian Address Details -->
-                                    <h3>Malaysian Address Details</h3>
-                                    <div class="form-group">
-                                        <label for="malaysianAddress">Malaysian Address:</label>
-                                        <textarea class="form-control" id="malaysianAddress" rows="2"></textarea>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="malaysianCountry">Malaysian Country:</label>
-                                        <input type="text" class="form-control" id="malaysianCountry" placeholder="Sarawak">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="malaysianPostcode">Malaysian Postcode:</label>
-                                        <input type="text" class="form-control" id="malaysianPostcode" placeholder="96100">
-                                    </div>
-
-                                    <!-- Contact Details -->
-                                    <div class="form-group">
-                                        <label for="overseasTelephone">Overseas Postcode:</label>
-                                        <input type="text" class="form-control" id="overseasTelephone" placeholder="012345678">
-                                    </div>
-
-                                    <!-- Contact Details -->
-                                    <h3>Contact Details</h3>
-                                    <div class="form-group">
-                                        <label for="malaysianTelephone">Malaysian Telephone:</label>
-                                        <input type="text" class="form-control" id="malaysianTelephone">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="overseasTelephone">Overseas Telephone:</label>
-                                        <input type="text" class="form-control" id="overseasTelephone">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="fax">Fax:</label>
-                                        <input type="text" class="form-control" id="fax">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="mobile">Mobile:</label>
-                                        <input type="text" class="form-control" id="mobile">
-                                    </div>
-
-                                    <!-- Visa/Citizenship Details -->
-                                    <h3>Visa Details</h3>
-                                    <div class="form-group">
-                                        <label for="countryOfCitizenship">Country of Citizenship:</label>
-                                        <input type="text" class="form-control" id="countryOfCitizenship">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="birthCountry">Birth Country:</label>
-                                        <input type="text" class="form-control" id="birthCountry">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="identityCardOrPassportNumber">Identity Card / Passport Number:</label>
-                                        <input type="text" class="form-control" id="identityCardOrPassportNumber">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="passportExpiry">Passport Expiry:</label>
-                                        <input type="text" class="form-control" id="passportExpiry">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="visaValidity">Visa Validity:</label>
-                                        <input type="text" class="form-control" id="visaValidity">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="visaType">Visa Type:</label>
-                                        <input type="text" class="form-control" id="visaType">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="visaExpiry">Visa Expiry:</label>
-                                        <input type="text" class="form-control" id="visaExpiry">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="visaCollectionLoaction">Visa Collection Location:</label>
-                                        <input type="text" class="form-control" id="visaCollectionLoaction">
-                                    </div>
-
-                                    <h3>Study Details</h3>
-                                    <div class="form-group">
-                                        <label for="firstName">Course Level</label>
-
-                                        <!-- select to be populated from database -->
-                                        <select class="form-control">
-                                            <option value="1">Foundation</option>
-                                            <option value="2">Diploma</option>
-                                            <option value="3">Degree</option>
-                                            <option value="4">Masters (by Coursework)</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="firstName">Course</label>
-                                        <!-- select to be populated from database -->
-                                        <select class="form-control">
-                                            <option value="1">Course 1</option>
-                                            <option value="2">Course 2</option>
-                                            <option value="3">Course 3</option>
-                                        </select>
-                                    </div>
-                                    <div id="courseCommencement">
-                                        <label for="courseCommencement">Course Commencement: </label>
-                                        <div class="input-group date">
-                                            <input type="text" class="form-control" >
-                                            <div class="input-group-addon">
-                                                <span class="glyphicon glyphicon-th"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <h3>Emergency Contact</h3>
-                                    <div class="form-group">
-                                        <label for="emergencyContactName">Emergency Contact Name:</label>
-                                        <input type="text" class="form-control" id="emergencyContactName">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emergencyContactAddress">Emergency Contact Address:</label>
-                                        <input type="text" class="form-control" id="emergencyContactAddress">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emergencyContactTelephone">Emergency Contact Telephone:</label>
-                                        <input type="text" class="form-control" id="emergencyContactTelephone">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emergencyContactFax">Emergency Contact Fax:</label>
-                                        <input type="text" class="form-control" id="emergencyContactFax">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="emergencyContactMobile">Emergency Contact Mobile:</label>
-                                        <input type="text" class="form-control" id="emergencyContactMobile">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emergencyContactEmail">Emergency Contact Email:</label>
-                                        <input type="text" class="form-control" id="emergencyContactEmail">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="emergencyContactRelationship">Emergency Contact Relationship:</label>
-                                        <input type="text" class="form-control" id="emergencyContactRelationship">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="EmergencyContactSpokenLanaguage">Emergency Contact Spoken Language:</label>
-                                        <input type="text" class="form-control" id="EmergencyContactSpokenLanaguage">
-                                    </div>
-
-                                    <h3>Student Accepted On: </h3>
-                                    <div id="acceptanceDate">
-                                        <div class="input-group date">
-                                            <input type="text" class="form-control" value="{{ $now }}">
-                                            <div class="input-group-addon">
-                                                <span class="glyphicon glyphicon-th"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-
                                 </div>
+
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-default">Submit</button>
+                                    <button type="button" class="btn btn-default create"
+                                        data-method="POST" data-url="{{ route('admin.managestudents.store') }}">
+                                        Submit
+                                    </button>
                                 </div>
                             </form>
                         </div> <!-- end. modal-content-->
@@ -309,18 +112,31 @@
 @stop
 
 @section('extra_js')
-<script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
 <script>
-$('#dateOfBirth .date').datepicker({
-    format: 'dd/mm/yyyy'
-});
+(function() {
+    // Get CSRF token
+    let getToken = function() {
+        return $('meta[name=_token]').attr('content')
+    }
 
-$('#courseCommencement .date').datepicker({
-    format: 'dd/mm/yyyy'
-});
+    $('.create').click(function() {
+        let method = $(this).data('method')
+        let url = $(this).data('url')
+        data = {
+            _token: getToken(),
+            studentID: $('#studentID').val(),
+            surname: $('#surname').val(),
+            givenName: $('#givenName').val(),
+            email: $('#email').val(),
+            courseCode: $('#courseCode').val()
+        }
 
-$('#acceptanceDate .date').datepicker({
-    format: 'dd/mm/yyyy'
-});
+        $.ajax({
+            'url': url,
+            'method': method,
+            'data': data
+        }).done(function(data) { window.location.reload() })
+    })
+}) ()
 </script>
 @stop

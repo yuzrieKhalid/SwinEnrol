30154bc4cf16e2198ed07af0523759e036fe6762
Mohamad Yuzrie Khalid <yuyieyuzrie@gmail.com>
2016-08-28 11:48:21 +0800

    student->enrolmenthistory. added exemption table and its controller

--- resources/views/student/enrolmenthistory.blade.php
+++ resources/views/student/enrolmenthistory.blade.php
@@ -26,7 +26,6 @@
                     <!-- Completed Units Table -->
                     <div class="table-responsive">
                         <table class="table">
-                            <caption><h3>Completed Units</h3></caption>
                             <thead>
                                 <th>Unit Code</th>
                                 <th>Unit Title</th>
@@ -56,54 +55,22 @@
                         </table>
                     </div>
 
-                    <!-- Ongoing Units Table -->
+                    <!-- Exempted Units -> only for students who applied for exemption -->
                     <div class="table-responsive">
                         <table class="table">
-                            <caption><h3>Current Semester Units</h3></caption>
+                            <caption><h3>Advanced Standing (Exemption)</h3></caption>
                             <thead>
                                 <th>Unit Code</th>
                                 <th>Unit Title</th>
-                                <th>Status</th>
                             </thead>
 
-                            @if(empty($enrolled))
-                                <tr><td colspan="3">No Units Enrolled Yet</td></tr>
+                            @if(empty($exempted))
+                                <tr><td colspan="2">No Units Exempted</td></tr>
                             @else
-                                @foreach ($enrolled as $unit)
+                                @foreach ($exempted as $unit)
                                     <tr>
                                         <td>{{ $unit->unitCode }}</td>
                                         <td>{{ $unit->unit->unitName }}</td>
-                                        @if($unit->status == 'confirmed')
-                                            <!-- Waiting to be approved (Phase 2) -->
-                                            <td><span class="glyphicon glyphicon glyphicon-alert" aria-hidden="true"></span></td>
-                                        @endif
-                                    </tr>
-                                @endforeach
-                            @endif
-                        </table>
-                    </div>
-
-                    <!-- Units Not Yet Taken / Failed Table -->
-                    <div class="table-responsive">
-                        <table class="table">
-                            <caption><h3>Not Completed Units</h3></caption>
-                            <thead>
-                                <th>Unit Code</th>
-                                <th>Unit Title</th>
-                                <th>Status</th>
-                            </thead>
-
-                            @if(empty($enrolled))
-                                <tr><td colspan="3">No Units Enrolled Yet</td></tr>
-                            @else
-                                @foreach ($enrolled as $unit)
-                                    <tr>
-                                        <td>{{ $unit->unitCode }}</td>
-                                        <td>{{ $unit->unit->unitName }}</td>
-                                        @if($unit->status == 'confirmed')
-                                            <!-- Is currently taken -->
-                                            <td><span class="glyphicon glyphicon glyphicon-remove" aria-hidden="true"></span></td>
-                                        @endif
                                     </tr>
                                 @endforeach
                             @endif

eb5dfe81ea3f312cb1ce30c96a9c6fe59e88fc95
Mohamad Yuzrie Khalid <yuyieyuzrie@gmail.com>
2016-09-20 13:21:14 +0800

    Renamed Admin - Resolve Issue : Admin - Resolve Leave of Absence

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ url('/admin') }}" class="list-group-item @if(Request::is('admin')) active @endif">Home</a>
         <a href="{{ url('/admin/managestudents') }}" class="list-group-item @if(Request::is('admin/managestudents*')) active @endif">Manage Students</a>
         <a href="{{ url('/admin/setenrolmentdates/create') }}" class="list-group-item @if(Request::is('admin/setenrolmentdates*')) active @endif">Set Enrolment Dates</a>
-        <a href="{{ url('/admin/resolveissue/create') }}" class="list-group-item @if(Request::is('admin/resolveissue*')) active @endif">Resolve Issues</a>
+        <a href="{{ url('/admin/resolveissue/create') }}" class="list-group-item @if(Request::is('admin/resolveissue*')) active @endif">Resolve Leave of Absence</a>
         <a href="{{ url('/admin/approvedissues/create') }}" class="list-group-item @if(Request::is('admin/approvedissues*')) active @endif">Approved Issues</a>
     </div>
 </div>

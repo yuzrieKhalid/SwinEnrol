0bd92a701fd13854d30812a02ec94c525430a962
Jason <j450n.tc@gmail.com>
2016-09-25 08:13:11 +0800

    student/viewunitlisting: added requisite columns

--- app/Http/Controllers/Student/ViewListingController.php
+++ app/Http/Controllers/Student/ViewListingController.php
@@ -12,22 +12,26 @@ use App\UnitTerm;
 use App\Config;
 use App\Course;
 use App\Student;
+use App\Requisite;
 
 use Carbon\Carbon;
 
 class ViewListingController extends Controller
 {
     public function index() {
-        $semester = Config::find('semester')->value;
-        $year = Config::find('year')->value;
-        $studyLevel = Course::find(Student::find(Auth::user()->username)->courseCode)->studyLevel;
+        $semester = Config::find('semester')->value; // get enrolment semester
+        $year = Config::find('year')->value; // get enrolment year
+        $studyLevel = Course::find(Student::find(Auth::user()->username)->courseCode)->studyLevel; // get student level of study
 
+        // get long semester unit listing
         $units = UnitTerm::with('unit', 'unit_type', 'course')
             ->where('unitType', '=', 'unit_listing')
             ->where('year', '=', $year)
             ->where('term', '=', $semester)
             ->where('enrolmentTerm', '=', 'long')
             ->get();
+
+        // return units that match study level
         $data['termUnits'] = [];
         foreach($units as $unit)
         {
@@ -35,17 +39,70 @@ class ViewListingController extends Controller
                 array_push($data['termUnits'], $unit);
         }
 
+        // get requisites
+        $requisites = Requisite::all();
+
+        // sort requisites
+        $termUnits = $data['termUnits'];
+        foreach($termUnits as $unit)
+        {
+            foreach($requisites as $requisite)
+            {
+                if($requisite->unitCode == $unit->unitCode)
+                {
+                    // prerequisite
+                    if($requisite->type == 'prerequisite')
+                        $data['requisite'][$unit->unitCode]['prerequisite'] = $requisite->requisite;
+
+                    // corequisite
+                    if($requisite->type == 'corequisite')
+                        $data['requisite'][$unit->unitCode]['corequisite'] = $requisite->requisite;
+
+                    // antirequisite
+                    if($requisite->type == 'antirequisite')
+                        $data['requisite'][$unit->unitCode]['antirequisite'] = $requisite->requisite;
+                }
+            }
+        }
+
+        // get short semester unit listing
         $units = UnitTerm::with('unit', 'unit_type')
             ->where('unitType', '=', 'unit_listing')
             ->where('year', '=', $year)
             ->where('term', '=', $semester)
             ->where('enrolmentTerm', '=', 'short')
             ->get();
+
+        // return units that match study level
         $data['termUnitsShort'] = [];
         foreach($units as $unit)
         {
             if($unit['course']->studyLevel == $studyLevel)
                 array_push($data['termUnitsShort'], $unit);
+        }
+
+        // get requisites
+        foreach($data['termUnitsShort'] as $unit)
+        {
+            $requisites[$unit->unitCode] = Requisite::where('unitCode', '=', $unit->unitCode)->get();
+            if(count($requisites[$unit->unitCode]) > 0)
+            {
+                // sort requisites by type
+                foreach($requisites[$unit->unitCode] as $requisite)
+                {
+                    // prerequisite
+                    if($requisite->type == 'prerequisite')
+                        $data['requisiteShort'][$unit->unitCode]['prerequisite'] = $requisite->requisite;
+
+                    // corequisite
+                    if($requisite->type == 'corequisite')
+                        $data['requisiteShort'][$unit->unitCode]['corequisite'] = $requisite->requisite;
+
+                    // antirequisite
+                    if($requisite->type == 'antirequisite')
+                        $data['requisiteShort'][$unit->unitCode]['antirequisite'] = $requisite->requisite;
+                }
+            }
         }
 
         return view('student.viewunitlistings', $data);
--- resources/views/student/viewunitlistings.blade.php
+++ resources/views/student/viewunitlistings.blade.php
@@ -21,13 +21,17 @@
                             <th>Unit Code</th>
                             <th>Unit Title</th>
                             <th>Pre-requisite</th>
+                            <th>Co-requisite</th>
+                            <th>Anti-requisite</th>
                         </thead>
                         {{-- Fetch data for unit listing (long semester) --}}
                         @foreach ($termUnits as $unit)
                         <tr>
                             <td>{{ $unit->unitCode }}</td>
                             <td>{{ $unit->unit->unitName }}</td>
-                            <td>{{ $unit['unit']->prerequisite }}</td>
+                            <td>@if(isset($requisite[$unit->unitCode]['prerequisite'])) {{ $requisite[$unit->unitCode]['prerequisite'] }} @else None @endif</td>
+                            <td>@if(isset($requisite[$unit->unitCode]['corequisite'])) {{ $requisite[$unit->unitCode]['corequisite'] }} @else None @endif</td>
+                            <td>@if(isset($requisite[$unit->unitCode]['antirequisite'])) {{ $requisite[$unit->unitCode]['antirequisite'] }} @else None @endif</td>
                         </tr>
                         @endforeach
                     </table>
@@ -40,13 +44,17 @@
                             <th>Unit Code</th>
                             <th>Unit Title</th>
                             <th>Pre-requisite</th>
+                            <th>Co-requisite</th>
+                            <th>Anti-requisite</th>
                         </thead>
                         {{-- Fetch data for unit listing (short semester) --}}
                         @foreach ($termUnitsShort as $unit)
                         <tr>
                             <td>{{ $unit->unitCode }}</td>
                             <td>{{ $unit->unit->unitName }}</td>
-                            <td>{{ $unit['unit']->prerequisite }}</td>
+                            <td>@if(isset($requisiteShort[$unit->unitCode]['prerequisite'])) {{ $requisiteShort[$unit->unitCode]['prerequisite'] }} @else None @endif</td>
+                            <td>@if(isset($requisiteShort[$unit->unitCode]['corequisite'])) {{ $requisiteShort[$unit->unitCode]['corequisite'] }} @else None @endif</td>
+                            <td>@if(isset($requisiteShort[$unit->unitCode]['antirequisite'])) {{ $requisiteShort[$unit->unitCode]['antirequisite'] }} @else None @endif</td>
                         </tr>
                         @endforeach
                     </table>

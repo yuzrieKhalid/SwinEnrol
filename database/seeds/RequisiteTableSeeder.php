d4c4621cfb8df3ab691336fbc061d2e11cf5793e
Jason <j450n.tc@gmail.com>
2016-09-25 08:09:43 +0800

    database/seeds: added created_at and updated_at columns to requisite table

--- database/seeds/RequisiteTableSeeder.php
+++ database/seeds/RequisiteTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class RequisiteTableSeeder extends Seeder
 {
@@ -16,85 +17,113 @@ class RequisiteTableSeeder extends Seeder
                 'unitCode' => 'HIT3158',
                 'requisite' => 'HIT3315',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT1234',
                 'requisite' => 'HIT1235',
                 'type' => 'antirequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT1234',
                 'requisite' => 'HIT1235',
                 'type' => 'antirequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'ACC10008',
                 'requisite' => 'ACC10007',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'COR30',
                 'requisite' => 'COR01',
                 'type' => 'corequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3172',
                 'requisite' => 'HIT3181',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3181',
                 'requisite' => 'HIT2080',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3309',
                 'requisite' => 'HIT3181',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT2312',
                 'requisite' => 'HIT1312',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT2308',
                 'requisite' => 'HIT3181',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3311',
                 'requisite' => 'HIT3309',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3258',
                 'requisite' => 'HIT3158',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3002',
                 'requisite' => 'HIT3172',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'unitCode' => 'HIT3037',
                 'requisite' => 'HIT3172',
                 'type' => 'prerequisite',
-                'conjunction' => 'AND'
+                'conjunction' => 'AND',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ]
         ]);
     }

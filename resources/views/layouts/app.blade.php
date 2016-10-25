7f25cd07529cb9bf3735d0c5686fcb8e50c09ed3
Mohamad Yuzrie Khalid <yuyieyuzrie@gmail.com>
2016-10-25 14:38:57 +0800

    updated the latest UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,67 +55,44 @@
                               <li><a title="Log Out" href="{{ url('/logout') }}">Log Out</a></li>
                               <li class="divider"></li>
                                 @if(Auth::user()->permissionLevel == '3')
-                                <li><a><b>Menu</b><span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                                <li class="dropdown-header">MENU</li>
                                 <li><a href="{{ url('/admin') }}">Home</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/admin/managestudents') }}">Manage Students</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/admin/setenrolmentdates/create') }}">Set Enrolment Dates</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/admin/resolveissue/create') }}">Resolve Leave of Absence</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/admin/approvedissues/create') }}">Approved Issues</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/admin/enrolmentstatus') }}">Enrolment Status</a></li>
                                 @endif
 
                                 @if(Auth::user()->permissionLevel == '1')
-                                <li><a><b>Menu</b><span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                                <li class="dropdown-header">MENU</li>
                                 <li><a href="{{ url('/student') }}">Enrolment Status</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/student/enrolmenthistory') }}">Enrolment History</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/student/manageunits/create') }}">Manage Units</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/student/viewstudyplanner') }}">View Study Planner</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/student/viewunitlistings') }}">View Unit Listings</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/student/enrolmentissues') }}">Other Enrolment Issues</a></li>
                                 @endif
 
                                 @if(Auth::user()->permissionLevel == '2')
-                                <li><a><b>Menu</b><span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                                <li class="dropdown-header">MENU</li>
                                 <li><a href="{{ url('/coordinator') }}">Home</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/coordinator/managestudyplanner/create') }}">Manage Study Planner</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/coordinator/manageunitlisting/create') }}">Manage Unit Listings</a></li>
-                                <li><li class="divider"></li>
                                 <li><a href="{{ url('/coordinator/manageunits/create') }}">Manage Units</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/coordinator/enrolmentamendment/create') }}">Resolve Enrolment Amendement</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/coordinator/resolveenrolmentissues/create') }}">Resolve Enrolment Issues</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/coordinator/graduationrequirements/create') }}">Graduation Requirements</a></li>
                                 @endif
 
                                 @if(Auth::user()->permissionLevel == '4')
-                                <li><a><b>Menu</b><span class="glyphicon glyphicon-cog pull-right"></span></a></li>
-
+                                <li class="dropdown-header">MENU</li>
                                 <li><a href="{{ url('/super') }}">Home</a></li>
-
                                 <li><a href="{{ url('/super/config') }}">Configuration</a>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/super/managecourse/create') }}">Manage Course</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/super/managestudentadmin') }}">Manage Student Administrators</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/super/managecoordinator') }}">Manage Course Coordinators</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/super/managestudent') }}">Manage Students</a></li>
-                                <li class="divider"></li>
                                 <li><a href="{{ url('/super/manageunittype') }}">Manage Unit Types</a></li>
                                 @endif
                             </ul>

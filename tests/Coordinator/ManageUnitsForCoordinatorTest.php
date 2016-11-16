25f42d00104a3abed3e8c8e138b2f4316048391c
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2016-11-17 04:48:04 +0800

    Test - Coordinator - [Remove and Update] Existing Unit

--- tests/Coordinator/ManageUnitsForCoordinatorTest.php
+++ tests/Coordinator/ManageUnitsForCoordinatorTest.php
@@ -56,4 +56,90 @@ class ManageUnitsForCoordinatorTest extends TestCase
         ->see('TestUnit1');
     }
 
+    /**
+     * JSON API Test
+     * SUCCESS TEST
+     * A test to edit an existing unit
+     * Condition: The unit has already exist in the record
+     * Environment: The unit has already exist in the record
+     */
+    public function testEditExisingUnit()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $coordinator = factory(App\CourseCoordinator::class)->create([
+            'username' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Unit
+        $unit = factory(App\Unit::class)->create([
+            'unitCode' => 'TESTCODE1',
+            'unitName' => 'TestUnit1',
+            'creditPoints' => '12.5',
+            'studyLevel' => 'Degree'
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // edit an existing unit
+        $this->json('PUT', 'coordinator/manageunits/TESTCODE1', [
+            'unitCode' => 'TESTCODE_EDITED1',
+            'unitName' => 'TestUnit1',
+            'creditPoints' => '12.5',
+            'studyLevel' => 'Degree'
+        ]);
+
+        // check if this unit has been successfully modified
+        $this->visit('coordinator/manageunits')
+        ->see('TESTCODE_EDITED1');
+    }
+
+    /**
+     * JSON API Test
+     * SUCCESS TEST
+     * A test to delete an existing unit
+     * Condition: The unit has already exist in the record
+     * Environment: The unit has already exist in the record
+     */
+    public function testRemoveExistingUnit()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $coordinator = factory(App\CourseCoordinator::class)->create([
+            'username' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Unit
+        $unit = factory(App\Unit::class)->create([
+            'unitCode' => 'TESTCODE1',
+            'unitName' => 'TestUnit1',
+            'creditPoints' => '12.5',
+            'studyLevel' => 'Degree'
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // check before deleting the unit
+        $this->visit('coordinator/manageunits')
+        ->see('TestUnit1');
+
+        // delete the unit
+        $this->json('DELETE', 'coordinator/manageunits/TESTCODE1');
+
+        // check if this unit has been removed
+        $this->visit('coordinator/manageunits')
+        ->dontsee('TestUnit1');
+    }
 }

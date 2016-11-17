7ad631f0a10c8c7c7e91137fc97c16ae7e8a39f9
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2016-11-17 12:08:39 +0800

    Test - Coordinator - [Submit (Pending), Approve and Disapprove] Exemption Issue

--- tests/Coordinator/EnrolmentIssuesForCoordinatorAndStudentTest.php
+++ tests/Coordinator/EnrolmentIssuesForCoordinatorAndStudentTest.php
@@ -20,6 +20,7 @@ class ManageUnitsForCoordinatorTest extends TestCase
         Artisan::call('migrate:rollback');
     }
 
+    // COURSE TRANSFER
     /**
      * JSON API Test
      * SUCCESS TEST
@@ -132,14 +133,163 @@ class ManageUnitsForCoordinatorTest extends TestCase
         ]);
     }
 
+    // EXEMPTION (ADVANCED STANDING)
+    /**
+     * JSON API Test
+     * SUCCESS TEST
+     * A test to submit Exemption [by Student]
+     * Condition: Exemption Form has not yet been submitted even once
+     * Environment: Exemption Form has not yet been submitted even once
+     */
+    public function testStudentSubmitExemption()
+    {
+        // Sample data - Student
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '1',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // submit exemption form and see if the form has been submitted
+        $this->json('POST', 'student/exemption', [
+            'issueID' => 2,
+            'submissionData' => "{\"fromProgramCode\":\"\",\"fromProgramTitle\":\"\",\"soughtUnitCode\":\"COS10009\",\"soughtUnitTitle\":\"asd\",\"exemptionUnitCodePrior\":\"TEST1\",\"exemptionUnitYearPrior\":\"2015\",\"exemptionUnitTitlePrior\":\"qwe\"}",
+            'attachmentData' => ''
+        ])->seeJson([
+            'studentID' => $student->studentID,
+            'issueID' => 2,
+            'status' => 'pending'
+        ]);
+    }
+
+    /**
+     * PAGE Test
+     * SUCCESS TEST
+     * A test to approve Exemption submitted by Student [by Coordinator]
+     * Condition: Exemption Form has been submitted from student
+     * Environment: Exemption Form has been submitted from student
+     */
+    public function testApproveExemptionRequest()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Exemption Issue
+        $exemption = factory(App\StudentEnrolmentIssues::class)->create([
+            'studentID' => $student->studentID,
+            'issueID' => 2,
+            'submissionData' => "{\"fromProgramCode\":\"\",\"fromProgramTitle\":\"\",\"soughtUnitCode\":\"COS10009\",\"soughtUnitTitle\":\"asd\",\"exemptionUnitCodePrior\":\"TEST1\",\"exemptionUnitYearPrior\":\"2015\",\"exemptionUnitTitlePrior\":\"qwe\"}",
+            'attachmentData' => ''
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // approve exemption
+        $this->json('PUT', 'coordinator/resolveenrolmentissues/'. $student->studentID .'/issue/2', [
+            'exemptionUnitCode' => 'COS10009',
+            'exemptionYear' => '2015'
+        ])->seeJson([
+            'status' => 'approved'
+        ]);
+    }
+
+    /**
+     * PAGE Test
+     * SUCCESS TEST
+     * A test to disapprove Exemption submitted by Student [by Coordinator]
+     * Condition: Exemption Form has been submitted from student
+     * Environment: Exemption Form has been submitted from student
+     */
+    public function testDisapproveExemptionRequest()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Exemption Issue
+        $exemption = factory(App\StudentEnrolmentIssues::class)->create([
+            'studentID' => $student->studentID,
+            'issueID' => 2,
+            'submissionData' => "{\"fromProgramCode\":\"\",\"fromProgramTitle\":\"\",\"soughtUnitCode\":\"COS10009\",\"soughtUnitTitle\":\"asd\",\"exemptionUnitCodePrior\":\"TEST1\",\"exemptionUnitYearPrior\":\"2015\",\"exemptionUnitTitlePrior\":\"qwe\"}",
+            'attachmentData' => ''
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // disapprove exemption
+        $this->json('DELETE', 'coordinator/resolveenrolmentissues/'. $student->studentID .'/issue/2')->seeJson([
+            'status' => 'disapproved'
+        ]);
+    }
+
+    // // LEAVE OF ABSENCE
+    // /**
+    //  * JSON API Test
+    //  * SUCCESS TEST
+    //  * A test to submit Course Transfer [by Student]
+    //  * Condition: Course Transfer Form has not yet been submitted even once
+    //  * Environment: Course Transfer Form has not yet been submitted even once
+    //  */
+    // public function testStudentSubmitCourseTransfer()
+    // {
+    //     // Sample data - Student
+    //     $course = factory(App\Course::class)->create();
+    //     $user = factory(App\User::class)->create([
+    //         'permissionLevel' => '1',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+    //     $student = factory(App\Student::class)->create([
+    //         'studentID' => $user->username,
+    //         'courseCode' => $course->courseCode
+    //     ]);
+    //
+    //     // login as coordinator
+    //     $this->actingAs($user);
+    //
+    //     // submit course transfer form and see if the form has been submitted
+    //     $this->json('POST', 'student/coursetransfer', [
+    //         'issueID' => 1,
+    //         'submissionData' => "{\"yearOfRequestedTransfer\":\"2016\",\"fromProgramCode\":\"I047\",\"fromProgramTitle\":\"Bachelor of Computer Science\",\"fromProgramIntakeYear\":\"2016\",\"toProgramCode\":\"BH-ERM\",\"toProgramTitle\":\"Business\",\"toProgramYear\":\"2016\",\"toReasons\":\"Sample to go Business\"}",
+    //         'attachmentData' => ""
+    //     ])->seeJson([
+    //         'studentID' => $student->studentID,
+    //         'issueID' => 1,
+    //         'status' => 'pending'
+    //     ]);
+    // }
+    //
     // /**
     //  * PAGE Test
     //  * SUCCESS TEST
-    //  * A test to remove existing graduationrequirements
-    //  * Condition: The selected requirement has been added to the course
-    //  * Environment: GraduationRequirements table contains an existing requirement
+    //  * A test to approve Course Transfer submitted by Student [by Coordinator]
+    //  * Condition: Course Transfer Form has been submitted from student
+    //  * Environment: Course Transfer Form has been submitted from student
     //  */
-    // public function testRemoveExistingRequirement()
+    // public function testApproveCourseTransferRequest()
     // {
     //     // Sample data - Course Coordinator
     //     $course = factory(App\Course::class)->create();
@@ -147,30 +297,65 @@ class ManageUnitsForCoordinatorTest extends TestCase
     //         'permissionLevel' => '2',
     //         'password' => bcrypt('123456'),
     //     ]);
-    //     $coordinator = factory(App\CourseCoordinator::class)->create([
-    //         'username' => $user->username,
+    //     $student = factory(App\Student::class)->create([
+    //         'studentID' => $user->username,
     //         'courseCode' => $course->courseCode
     //     ]);
     //
-    //     // Sample Requirement
-    //     $gradrequirement = factory(App\GraduationRequirements::class)->create([
-    //         'courseCode' => $course->courseCode,
-    //         // 'unitType' => 'Core', 'unitCount' => '20',
+    //     // Sample Course Transfer Issue
+    //     $coursetransfer = factory(App\StudentEnrolmentIssues::class)->create([
+    //         'studentID' => $student->studentID,
+    //         'issueID' => 1,
+    //         'submissionData' => "{\"yearOfRequestedTransfer\":\"2016\",\"fromProgramCode\":\"I047\",\"fromProgramTitle\":\"Bachelor of Computer Science\",\"fromProgramIntakeYear\":\"2016\",\"toProgramCode\":\"BH-ERM\",\"toProgramTitle\":\"Business\",\"toProgramYear\":\"2016\",\"toReasons\":\"Sample to go Business\"}",
+    //         'attachmentData' => ""
     //     ]);
     //
     //     // login as coordinator
     //     $this->actingAs($user);
     //
-    //     // check before deleting the unit
-    //     // Core still can be seen from <select>
-    //     $this->visit('coordinator/graduationrequirements/create')
-    //     ->see('20');
+    //     // edit an existing requirement
+    //     $this->json('PUT', 'coordinator/resolveenrolmentissues/'. $student->studentID .'/issue/1', [
+    //         'proposedProgramCode' => 'BH-ERM',
+    //         'proposedIntakeYear' => '2016'
+    //     ])->seeJson([
+    //         'status' => 'approved'
+    //     ]);
+    // }
     //
-    //     // delete the unit
-    //     $this->json('DELETE', 'coordinator/graduationrequirements/Core');
+    // /**
+    //  * PAGE Test
+    //  * SUCCESS TEST
+    //  * A test to disapprove Course Transfer submitted by Student [by Coordinator]
+    //  * Condition: Course Transfer Form has been submitted from student
+    //  * Environment: Course Transfer Form has been submitted from student
+    //  */
+    // public function testDisapproveCourseTransferRequest()
+    // {
+    //     // Sample data - Course Coordinator
+    //     $course = factory(App\Course::class)->create();
+    //     $user = factory(App\User::class)->create([
+    //         'permissionLevel' => '2',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+    //     $student = factory(App\Student::class)->create([
+    //         'studentID' => $user->username,
+    //         'courseCode' => $course->courseCode
+    //     ]);
     //
-    //     // check if this unit has been removed
-    //     $this->visit('coordinator/graduationrequirements/create')
-    //     ->dontsee('20');
+    //     // Sample Course Transfer Issue
+    //     $coursetransfer = factory(App\StudentEnrolmentIssues::class)->create([
+    //         'studentID' => $student->studentID,
+    //         'issueID' => 1,
+    //         'submissionData' => "{\"yearOfRequestedTransfer\":\"2016\",\"fromProgramCode\":\"I047\",\"fromProgramTitle\":\"Bachelor of Computer Science\",\"fromProgramIntakeYear\":\"2016\",\"toProgramCode\":\"BH-ERM\",\"toProgramTitle\":\"Business\",\"toProgramYear\":\"2016\",\"toReasons\":\"Sample to go Business\"}",
+    //         'attachmentData' => ""
+    //     ]);
+    //
+    //     // login as coordinator
+    //     $this->actingAs($user);
+    //
+    //     // edit an existing requirement
+    //     $this->json('DELETE', 'coordinator/resolveenrolmentissues/'. $student->studentID .'/issue/1')->seeJson([
+    //         'status' => 'disapproved'
+    //     ]);
     // }
 }

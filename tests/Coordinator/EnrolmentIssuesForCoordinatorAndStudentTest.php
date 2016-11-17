3a9c5ec7a7f592de54f1b164892bc641a3a8e4ea
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2016-11-17 13:21:48 +0800

    Test - Coordinator - [Submit (Pending), Approve and Disapprove] Amendment Add/Drop Issue

--- tests/Coordinator/EnrolmentIssuesForCoordinatorAndStudentTest.php
+++ tests/Coordinator/EnrolmentIssuesForCoordinatorAndStudentTest.php
@@ -359,4 +359,299 @@ class ManageUnitsForCoordinatorTest extends TestCase
             'status' => 'disapproved'
         ]);
     }
+
+    // RE-ENROLMENT AMENDMENT
+    /**
+     * JSON API Test
+     * SUCCESS TEST
+     * A test to submit Exemption [by Student]
+     * Condition: Exemption Form has not yet been submitted even once
+     * Environment: Exemption Form has not yet been submitted even once
+     */
+    public function testStudentSubmitAmendmentAdd()
+    {
+        // Sample data - Student
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '1',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // submit exemption form and see if the form has been submitted
+        $this->json('PUT', 'student/manageunits/COS10004', [
+            'unitCode' => 'COS10004',
+            'semesterLength' => 'long',
+            'reason' => 'Sample Reason',
+            'status' => 'pending_add'
+        ]);
+    }
+
+    /**
+     * PAGE Test
+     * SUCCESS TEST
+     * A test to approve Amendment submitted by Student [by Coordinator]
+     * Condition: Amendment Form has been submitted from student
+     * Environment: Amendment Form has been submitted from student
+     */
+    public function testApproveAmendmentAddRequest()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => '123456789',
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Amendment Issue
+        $unit = factory(App\Unit::class)->create();
+        $amendmentunit = factory(App\EnrolmentUnits::class)->create([
+            'studentID' => $student->studentID,
+            'unitCode' => $unit->unitCode,
+            'semesterLength' => 'long',
+            'status' => 'pending_add'
+        ]);
+        $amendmentissue = factory(App\StudentEnrolmentIssues::class)->create([
+            'studentID' => $student->studentID,
+            'issueID' => 4,
+            'status' => $amendmentunit->status,
+            'submissionData' => 'Sample Reason.',
+            'attachmentData' => $amendmentunit->unitCode
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // before approving, confirm the issue exist
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([
+            'studentID' => $student->studentID,
+            'status' => 'pending_add'
+        ]);
+
+        // approve exemption
+        $this->json('PUT', 'coordinator/enrolmentamendment/'. $student->studentID .'unit/'. $amendmentunit->unitCode . '', [
+            'status' => 'pending_add'
+        ]);
+
+        // status has changed to 'confirmed'
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([]);
+    }
+
+    /**
+     * PAGE Test
+     * SUCCESS TEST
+     * A test to disapprove Amendment submitted by Student [by Coordinator]
+     * Condition: Amendment has been submitted from student
+     * Environment: Amendment Form has been submitted from student
+     */
+    public function testDisapproveAmendmentAddRequest()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Amendment Issue
+        $unit = factory(App\Unit::class)->create();
+        $amendmentunit = factory(App\EnrolmentUnits::class)->create([
+            'studentID' => $student->studentID,
+            'unitCode' => $unit->unitCode,
+            'semesterLength' => 'long',
+            'status' => 'pending_add'
+        ]);
+        $amendmentissue = factory(App\StudentEnrolmentIssues::class)->create([
+            'studentID' => $student->studentID,
+            'issueID' => 4,
+            'status' => $amendmentunit->status,
+            'submissionData' => 'Sample Reason.',
+            'attachmentData' => $amendmentunit->unitCode
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // before approving, confirm the issue exist
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([
+            'studentID' => $student->studentID,
+            'status' => 'pending_add'
+        ]);
+
+        // approve exemption
+        $this->json('DELETE', 'coordinator/enrolmentamendment/'. $student->studentID .'unit/'. $amendmentunit->unitCode . '', [
+            'status' => 'pending_add'
+        ]);
+
+        // status has changed to 'confirmed'
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([]);
+    }
+
+    /**
+     * JSON API Test
+     * SUCCESS TEST
+     * A test to submit Amendment [by Student]
+     * Condition: Amendment Form has not yet been submitted even once
+     * Environment: Amendment Form has not yet been submitted even once
+     */
+    public function testStudentSubmitAmendmentDrop()
+    {
+        // Sample data - Student
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '1',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        $unit = factory(App\Unit::class)->create();
+        $amendmentdrop = factory(App\EnrolmentUnits::class)->create([
+            'studentID' => $student->studentID,
+            'unitCode' => $unit->unitCode,
+            'status' => 'pending'
+        ]);
+        // login as coordinator
+        $this->actingAs($user);
+
+        // submit exemption form and see if the form has been submitted
+        $this->json('PUT', 'student/manageunits/' . $amendmentdrop->unitCode . '', [
+            'unitCode' => $amendmentdrop->unitCode,
+            'semesterLength' => 'long',
+            'reason' => 'Sample Reason',
+            'status' => 'pending_drop'
+        ]);
+    }
+
+    /**
+     * PAGE Test
+     * SUCCESS TEST
+     * A test to approve Amendment submitted by Student [by Coordinator]
+     * Condition: Amendment Form has been submitted from student
+     * Environment: Amendment Form has been submitted from student
+     */
+    public function testApproveAmendmentDropRequest()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => '123456789',
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Amendment Issue
+        $unit = factory(App\Unit::class)->create();
+        $amendmentunit = factory(App\EnrolmentUnits::class)->create([
+            'studentID' => $student->studentID,
+            'unitCode' => $unit->unitCode,
+            'semesterLength' => 'long',
+            'status' => 'pending_drop'
+        ]);
+        $amendmentissue = factory(App\StudentEnrolmentIssues::class)->create([
+            'studentID' => $student->studentID,
+            'issueID' => 4,
+            'status' => $amendmentunit->status,
+            'submissionData' => 'Sample Reason.',
+            'attachmentData' => $amendmentunit->unitCode
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // before approving, confirm the issue exist
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([
+            'studentID' => $student->studentID,
+            'status' => 'pending_drop'
+        ]);
+
+        // approve exemption
+        $this->json('PUT', 'coordinator/enrolmentamendment/'. $student->studentID .'unit/'. $amendmentunit->unitCode . '', [
+            'status' => 'pending_drop'
+        ]);
+
+        // status has changed to 'confirmed'
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([]);
+    }
+
+    /**
+     * PAGE Test
+     * SUCCESS TEST
+     * A test to disapprove Amendment submitted by Student [by Coordinator]
+     * Condition: Amendment has been submitted from student
+     * Environment: Amendment Form has been submitted from student
+     */
+    public function testDisapproveAmendmentDropRequest()
+    {
+        // Sample data - Course Coordinator
+        $course = factory(App\Course::class)->create();
+        $user = factory(App\User::class)->create([
+            'permissionLevel' => '2',
+            'password' => bcrypt('123456'),
+        ]);
+        $student = factory(App\Student::class)->create([
+            'studentID' => $user->username,
+            'courseCode' => $course->courseCode
+        ]);
+
+        // Sample Amendment Issue
+        $unit = factory(App\Unit::class)->create();
+        $amendmentunit = factory(App\EnrolmentUnits::class)->create([
+            'studentID' => $student->studentID,
+            'unitCode' => $unit->unitCode,
+            'semesterLength' => 'long',
+            'status' => 'pending_drop'
+        ]);
+        $amendmentissue = factory(App\StudentEnrolmentIssues::class)->create([
+            'studentID' => $student->studentID,
+            'issueID' => 4,
+            'status' => $amendmentunit->status,
+            'submissionData' => 'Sample Reason.',
+            'attachmentData' => $amendmentunit->unitCode
+        ]);
+
+        // login as coordinator
+        $this->actingAs($user);
+
+        // before approving, confirm the issue exist
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([
+            'studentID' => $student->studentID,
+            'status' => 'pending_drop'
+        ]);
+
+        // approve exemption
+        $this->json('DELETE', 'coordinator/enrolmentamendment/'. $student->studentID .'unit/'. $amendmentunit->unitCode . '', [
+            'status' => 'pending_drop'
+        ]);
+
+        // status has changed to 'confirmed'
+        $this->visit('coordinator/enrolmentamendment')
+        ->seeJson([]);
+    }
 }
